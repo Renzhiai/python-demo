8fc1e4a276f5789149c6d69ea39cd53bb85e0b93
renzhiai <Administrator@PC-20150905NHUU>
2017-12-23 18:32:57 +0800

    bootstrap_1223

--- test.py
+++ test.py
@@ -1,19 +1,20 @@
 # coding:utf-8
-import mock
-import unittest
 
-from test2 import Count
+def groupByElement(lst):
+    ''' 元素分组 '''
+    result = [[]]
+    length = len(lst)
+    i = 0
+    for i in range(length-1):
+        if lst == lst[i + 1]:
+            result[-1].append(lst)
+        else:
+            result[-1].append(lst)
+            result.append([])
 
-# test Count class
-class TestCount(unittest.TestCase):
+    result[-1].append(lst)
+    return result
 
-    def test_add(self):
-        count = Count()
-        count.add = mock.Mock(return_value=13,side_effect=count.add)
-        result = count.add(7,7)
-        print(result)
-        count.add.assert_called_with(7,7)
-        self.assertEqual(result,13)
-
-if __name__ == '__main__':
-    unittest.main()
+if __name__=='__main__':
+    l=[1,2,2,3,4,2,5,6]
+    print(groupByElement(l))
