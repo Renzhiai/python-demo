6450b27c4d161d48957dd5663694cc31765381f3
404261318@qq.com <404261318@qq.com>
2018-03-07 19:47:31 +0800

    update_0307

--- beautifulsoup/test16_form.py
+++ beautifulsoup/test16_form.py
@@ -8,6 +8,6 @@ _url='http://192.168.0.96/yihao01-park-sso/login?service=http%3A%2F%2F192.168.0.
 # params={'firstname':'Ren','lastname':'Zhiai'}
 # r=requests.post('http://pythonscraping.com/files/processing.php',data=params)
 # print(r.text)
-params={'username':'admin1037','password':'a123456','captcha':'111111'}
+params={'username':'admin1037','password':'a123456','captcha':'123456'}
 r=requests.post(_url,data=params)
 print(r._content)
--- power_consumption/06_battery.py
+++ power_consumption/06_battery.py
@@ -11,32 +11,3 @@ adb shell dumpsys batterystats >d:/battery.txt
 根据uid 找到耗费的电量
 '''
 
-#要测试的包
-package_name='com.oecommunity.oeshop'
-#要测试的activity
-activity_name='.component.main.activity.MainActivity'
-#执行的命令
-cmd='adb shell am start -W '+package_name+'/'+activity_name+' | find "TotalTime"'
-proc=subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.STDOUT,shell=True)
-# print(result)
-result=proc.communicate()[0].split('\n')
-start_time=''
-#从打印结果来看，最后一个是空字符串，所以删除
-result.pop()
-# print(result)
-stime=result[0]
-#如果TotalTime在结果里
-if 'TotalTime' in stime:
-    #结果格式为：TotalTime: xxxx，从结果第11位开始拼接数字
-    for char in stime[11:]:
-        #如果是数字就拼接，不是就跳出
-        if char.isdigit():
-            start_time=start_time+char
-        else:
-            break
-else:
-    print(u'没有找到TotalTime')
-if start_time=='':
-    print(u'获取启动时间失败')
-else:
-    print(start_time)
