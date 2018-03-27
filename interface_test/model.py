d54bb2b3acfee9e1535f2b7b14fd8b1b8718d4a7
rza <404261318@qq.com>
2018-03-27 18:14:59 +0800

    updateFlask

--- interface_test/model.py
+++ interface_test/model.py
@@ -2,145 +2,60 @@
 import requests
 
 cookie_28='plf=/; route=6a2329437bbf04787f355e73aee70557; JSESSIONID=E26C2D4E9BA9EC592AFB662ACA289C40; eid01=wKgAyFnxfpl3DG0GAx5jAg=='
-cookie_106='plf=/; JSESSIONID=2977A3F832B7E9761263836CC2886332; eid01=wKgAalnxrrMRwBxQAyhdAg=='
+cookie_106='JSESSIONID=FD9498FE3D93CBE48764F0DEE70FAB33; eid01=wKgAalqx+H8R8UWzAwSqAg==; TOKEN=43a039766213a5e36ec6e05edb897cc2_f6c80e3f0f4a499c89a57f4026be2362'
 addr_28=''
-addr_106=''
-
+addr_106='https://testone.0easy.com'
+addr=addr_106
+cookie=cookie_106
 #添加门禁
-def add_door_28():
-    url=addr_28+'url'
+def add_door():
+    url=addr+'/yihao01-ecommunity-cloud/manage/doorAction!addDoor.do'
     dict_all={
-        'doorDTO.doorModel.id':'',
-        'doorDTO.doorModel.adrWifiPowerBase':'-55',
-        'doorDTO.doorModel.adrBtPowerBase':'-100',
-        'doorDTO.doorModel.iosBtPowerBase':'-100',
-        'doorDTO.doorModel.propId':'2806',
-        'doorDTO.doorModel.doorName':'interface_test',
-        'doorDTO.doorModel.doorCode':'254',
-        'doorDTO.doorModel.version':'OEASY-X1',
-        'doorDTO.doorModel.doorType':'1',
-        'doorDTO.doorModel.doorPublic':'2',
-        'doorDTO.doorModel.createPwd':'0',
-        'doorDTO.doorModel.sn':'',
-        'doorDTO.doorModel.adrWifiPower':'-55',
-        'doorDTO.doorModel.adrBtPower':'-100',
-        'doorDTO.doorModel.iosBtPower':'-100'
+        'doorModel.id':'',
+        'doorModel.adrWifiPowerBase':'-55',
+        'doorModel.adrBtPowerBase':'-100',
+        'doorModel.iosBtPowerBase':'-100',
+        'doorModel.propId':'2806',
+        'doorModel.doorName':'interfaceTest',
+        'doorModel.doorCode':'211',
+        'doorModel.version':'OEASY-X1',
+        'doorModel.doorType':'1',
+        'doorModel.doorPublic':'2',
+        'doorModel.createPwd':'0',
+        'doorModel.sn':'',
+        'doorModel.adrWifiPower':'-55',
+        'doorModel.adrBtPower':'-100',
+        'doorModel.iosBtPower':'-100'
     }
 
     # cafile = 'cacert.pem' # http://curl.haxx.se/ca/cacert.pem
     # https://stackoverflow.com/questions/10667960/python-requests-throwing-sslerror
-    result=requests.post(url,params=dict_all,headers={'Cookie':cookie_28},verify=False)
-    if '门禁编码 [ 254 ] 已经使用，请更换门禁编码' in result.content:
+    result=requests.post(url,params=dict_all,headers={'Cookie':cookie},verify=False)
+    print(result.content.decode('utf-8'))
+    if '门禁编码 [ 211 ] 已经使用，请更换门禁编码' in result.content:
         return u'门禁添加成功'
     else:
         return u'门禁添加失败'
 
-#打开开门语音
-def open_door_voice_28():
-    url=addr_28+'url'
-    dict_all={
-        'status':'1',
-        'content':'祝您出入平安',
-        'id':'287'
-    }
-
-    result=requests.get(url,params=dict_all,headers={'Cookie':cookie_28},verify=False)
-    if '0'==result.content:
-        return u'开门语音打开成功'
-    else:
-        return u'开门语音打开失败'
-
 #添加门禁工程师
-def add_door_engineer_28():
-    url=addr_28+'/yihao01-ecommunity-cloud/manage/doorEngineerAction!addDoorEngineer.do'
+def add_door_engineer():
+    url=addr+'/yihao01-ecommunity-cloud/manage/doorEngineerAction!addDoorEngineer.do'
     dict_all={
-        'doorEngineer.id':'',
-        'doorEngineer.userAccount':'interface',
-        'doorEngineer.password':'interface',
-        'doorEngineer.name':'rzatest',
-        'doorEngineer.type':'1',
-        'doorEngineer.telephone':'17722402544',
-        'doorEngineer.idCard':'',
-        'doorEngineer.startTime':'2017-11-23',
-        'doorEngineer.endTime':'2017-12-01',
-        'doorEngineer.remark':''
+        'id':'',
+        'userAccount':'interface',
+        'password':'interface',
+        'name':'rzatest',
+        'type':'1',
+        'telephone':'17722402544',
+        'idCard':'',
+        'startTime':'2017-11-23',
+        'endTime':'2017-12-01',
+        'remark':''
     }
 
-    result=requests.post(url,params=dict_all,headers={'Cookie':cookie_28},verify=False)
+    result=requests.post(url,params=dict_all,headers={'Cookie':cookie},verify=False)
+    print(result.content.decode('utf-8'))
     if '账号重复' in result.content:
         return u'门禁工程师添加成功'
     else:
         return u'门禁工程师添加失败'
-
-#添加可视对讲
-def add_visible_device_28():
-    url=addr_28+'/yihao01-ecommunity-cloud/manage/talkbackAction!saveDevice.do'
-    dict_all={
-        'operatorType':'1',
-        'device.prop_id':'2806',
-        'device.dev_type':'1',
-        'device.name':'20171213',
-        'device.dev_sn':'20171213',
-        'device.dev_mac':'1EED19000000',
-        'device.building_code':'0101',
-        'device.extension_code':'87',
-        'device.dev_swver':'',
-        'device.description':'',
-        'doorDTO.doorModel.id':'',
-        'doorDTO.doorModel.adrWifiPowerBase':'-55',
-        'doorDTO.doorModel.adrBtPowerBase':'-100',
-        'doorDTO.doorModel.iosBtPowerBase':'-100',
-        'doorDTO.doorModel.version':'OEASY-Q3',
-        'doorNamePrefix':u'对讲门禁：1栋1单元',
-        'doorDTO.doorModel.doorName':'20171213',
-        'doorDTO.doorModel.doorCode':'203',
-        'doorDTO.doorModel.doorCodeChild':'87',
-        'doorDTO.doorModel.doorType':'1',
-        'doorDTO.doorModel.doorPublic':'2',
-        'doorDTO.doorModel.createPwd':'0',
-        'doorDTO.doorModel.sn:,doorDTO.doorModel.adrWifiPower':'-55',
-        'doorDTO.doorModel.adrBtPower':'-100',
-        'doorDTO.doorModel.iosBtPower':'-100'
-    }
-
-    result=requests.post(url,params=dict_all,headers={'Cookie':cookie_28},verify=False)
-    if '门禁机号+门禁分机号是小区内门禁的标识，必须唯一' in result.content:
-        return u'可视对讲添加成功'
-    else:
-        return u'可视对讲添加失败'
-
-def add_visible_device_106():
-    url=addr_106+'/yihao01-ecommunity-cloud/manage/talkbackAction!saveDevice.do'
-    dict_all={
-        'operatorType':'1',
-        'device.prop_id':'5555',
-        'device.dev_type':'1',
-        'device.name':'20171213',
-        'device.dev_sn':'20171213',
-        'device.dev_mac':'1EED19000000',
-        'device.building_code':'0101',
-        'device.extension_code':'71',
-        'device.dev_swver':'',
-        'device.description':'',
-        'doorDTO.doorModel.id':'',
-        'doorDTO.doorModel.adrWifiPowerBase':'-55',
-        'doorDTO.doorModel.adrBtPowerBase':'-100',
-        'doorDTO.doorModel.iosBtPowerBase':'-100',
-        'doorDTO.doorModel.version':'OEASY-Q1',
-        'doorNamePrefix':u'对讲门禁：1栋1单元',
-        'doorDTO.doorModel.doorName':'20171213',
-        'doorDTO.doorModel.doorCode':'201',
-        'doorDTO.doorModel.doorCodeChild':'71',
-        'doorDTO.doorModel.doorType':'1',
-        'doorDTO.doorModel.doorPublic':'2',
-        'doorDTO.doorModel.createPwd':'0',
-        'doorDTO.doorModel.sn':'',
-        'doorDTO.doorModel.adrWifiPower':'-55',
-        'doorDTO.doorModel.adrBtPower':'-100',
-        'doorDTO.doorModel.iosBtPower':'-100',
-    }
-    result=requests.post(url,params=dict_all,headers={'Cookie':cookie_106},verify=False)
-    if '门禁机号+门禁分机号是小区内门禁的标识，必须唯一' in result.content:
-        return u'可视对讲添加成功'
-    else:
-        return u'可视对讲添加失败'
