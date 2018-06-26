08762f7be5ec9505cbf7f589d774189a9175751d
rza <404261318@qq.com>
2018-06-26 19:04:59 +0800

    update 0626

--- beautifulsoup/dealData.py
+++ beautifulsoup/dealData.py
@@ -1,22 +1,35 @@
 # coding:utf-8
 import requests,time
-from bs4 import BeautifulSoup
 import re
+import warnings
+warnings.filterwarnings("ignore")
 
-cookie28 = 'plf=/manage/systemAction!centerInfoShow.do; JSESSIONID=79E5EAAEB94221AEE4E399A99303DBF9; eid01=wKgAyFqxsvsYJktrPLSnAg==; TOKEN=fdb8a8cb7d3eaa2a4d3c1c2b1c8d14c0_91493a9d751d40c9a28aed0ac13eb1bd'
+userIds = []
+cookie28 = 'plf=/manage/systemAction!centerInfoShow.do; JSESSIONID=7D10B4264F18195EC968FCF8FD59287F; eid01=wKgAyFqxsvsYJktrPLSnAg==; TOKEN=fdb8a8cb7d3eaa2a4d3c1c2b1c8d14c0_cdcd424abb6e407685ec5ba261036bfc'
 url = 'https://01.0easy.com/yihao01-ecommunity-cloud/manage/houseUserAction!findHouseUserList.do'
-result = requests.get(url,headers={'Cookie': cookie28})
-# print(result.content)
+result = requests.get(url,headers={'Cookie': cookie28},verify=False)
 pageNum = ''
-for line in result.content.decode('utf-8').split('\n'):
-    if re.findall(re.compile('条(.*?)页'),line):
-        for s in re.findall(re.compile('条(.*?)页'),line)[0]:
+for line1 in result.content.decode('utf-8').split('\n'):
+    if re.findall(re.compile('条(.*?)页'),line1):
+        for s in re.findall(re.compile('条(.*?)页'),line1)[0]:
             if s.isdigit():
                 pageNum = pageNum + s
-print(pageNum)
-    # print(re.compile('条(.*?)页').findall(line))
-    # if "detail('" in line:
-    #     startIndex = line.find("detail('")
-    #     endIndex = line.find("')\" value")
-    #     print(line[startIndex+8:endIndex])
-    #     print(line)
+if pageNum.isdigit():
+    for i in range(1,int(pageNum)):
+        time.sleep(0.1)
+        offset = (int(i) - 1) * 10
+        url = 'https://01.0easy.com/yihao01-ecommunity-cloud/manage/houseUserAction!findHouseUserList.do?pager.offset='+str(offset)
+        result = requests.get(url, headers={'Cookie': cookie28},verify=False)
+        for line in result.content.decode('utf-8').split('\n'):
+            if "detail('" in line:
+                startIndex = line.find("detail('")
+                endIndex = line.find("')\" value")
+                userIds.append(line[startIndex+8:endIndex])
+i = 0
+for id in userIds[1:]:
+    url = 'https://01.0easy.com/yihao01-ecommunity-cloud/manage/houseUserAction!deleteUser.do?id='+id
+    result = requests.get(url, headers={'Cookie': cookie28}, verify=False)
+    i = i + 1
+    print(result.status_code)
+    print('删除第'+str(i)+'个')
+    time.sleep(0.1)
